f1465f297d3e80e1ac1164cc7701bc0b3684fee0
vibinsgit <vibinsv810@gmail.com>
2024-07-21 22:06:45 +0530

    Bubble sort algo added

---
+++ Kunal_Lecture/L17(BubbleSortAlgorithm)/BubbleSort.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 5, 4, 2, 3};
+        int[] arr2 = {1, 2, 3, 4, 5};
+
+        sort(arr2);
+        System.out.println(Arrays.toString(arr2));
+    }
+
+    static void sort(int[] arr) {
+        boolean swap;
+        for(int i = 0; i < arr.length; i++){
+             swap = false;
+            for(int j = 1; j < arr.length - i; j++){
+                if(arr[j] < arr[j - 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swap = true;
+                }
+                if(!swap){
+                    break;
+                }
+            }
+        }
+    }
+}
